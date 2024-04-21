18c7b9e57f613ef4b77e20e46e1d7c93439798bd
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2024-04-21 14:02:26 +0530

    comitt

--- Exp4_CRC/CRC.py
+++ Exp4_CRC/CRC.py
@@ -1,24 +1,73 @@
+# Function that performs XOR operation
+def XOR(check_value, gen_poly):
+    # If both bits are the same, the output is 0
+    # If the bits are different the output is 1
+    for j in range(1, len(gen_poly)):
+        check_value[j] = '0' if check_value[j] == gen_poly[j] else '1'
+
+# Function to check for errors on the receiver side
+def receiver(data, gen_poly):
+    # Get the received data
+    print("Enter the received data: ")
+    received_data = input()
+    print("\n-----------------------------")
+    print("Data received: ", received_data)
+
+    # Cyclic Redundancy Check
+    check_value = crc(data, gen_poly)
+
+    # Check if the remainder is zero to find the error
+    for i in range(len(gen_poly) - 1):
+        if check_value[i] != '0':
+            print("\nError detected\n")
+            return
+    print("\nNo error detected\n")
+
+# Cyclic Redundancy Check function
+def crc(data, gen_poly):
+    check_value = list(data)
+    i = 0
+    while i <= len(data) - len(gen_poly) + 1:
+        # Check if the first bit is 1 and call XOR function
+        if check_value[0] == '1':
+            XOR(check_value, gen_poly)
+        # Move the bits by 1 position for the next computation
+        check_value = check_value[1:] + [data[i + len(gen_poly) - 1]]
+        i += 1
+    return check_value
+
 def main():
-    w = int(input("Enter window size: "))
-    f = int(input("\nEnter number of frames to transmit: "))
+    # Get the data to be transmitted
+    data = input("\nEnter data to be transmitted: ")
+    # Get the generator polynomial
+    gen_poly = input("\nEnter the Generating polynomial: ")
 
-    frames = []
-    print("\nEnter {} frames: ".format(f))
-    for i in range(1, f + 1):
-        frames.append(int(input()))
+    # Find the length of data
+    data_length = len(data)
+    # Appending n-1 zeros to the data
+    data += '0' * (len(gen_poly) - 1)
 
-    print("\nWith sliding window protocol the frames will be sent in the following manner (assuming no corruption of frames)\n")
-    print("After sending {} frames at each stage sender waits for acknowledgement sent by the receiver\n".format(w))
+    print("\n----------------------------------------")
+    # Print the data with padded zeros
+    print("\nData padded with n-1 zeros:", data)
+    print("\n----------------------------------------")
 
-    for i in range(0, f):
-        if i % w == 0:
-            print(frames[i])
-            print("Acknowledgement of above frames sent is received by sender\n")
-        else:
-            print(frames[i])
+    # Cyclic Redundancy Check
+    check_value = crc(data, gen_poly)
 
-    if f % w != 0:
-        print("\nAcknowledgement of above frames sent is received by sender")
+    # Print the computed check value
+    print("\nCRC or Check value is:", ''.join(check_value))
+
+    # Append data with check_value(CRC)
+    data += ''.join(check_value[len(data) - data_length:])
+
+    print("\n----------------------------------------")
+    # Print the final data to be sent
+    print("\nFinal data to be sent:", data)
+    print("\n----------------------------------------")
+
+    # Calling the receiver function to check errors
+    receiver(data, gen_poly)
 
 if __name__ == "__main__":
     main()
